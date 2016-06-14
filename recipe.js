c6a13f600d7775b6ca6442201d8845ba154c24a3
corya0687 <corya0687@gmail.com>
2016-06-14 18:20:53 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -2,5 +2,21 @@
 
 function addIngredient(recipe, ingredientName, amounts) {
   recipe[ingredientName] = amounts;
-  return recipe
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredientName) {
+  delete recipe[ingredientName];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredientName, amounts) {
+  recipe[ingredientName] = amounts;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var ingredientName in recipe){
+    console.log("this recipe calls for " +recipe[ingredientName] + " of " + ingredientName )
+  };
 }
